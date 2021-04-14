3ea5b55fbc0178bc337d12b263617eb919e67e9e
Avita India <avitaind@gmail.com>
2021-04-14 12:35:35 +0530

    Changes

--- resources/views/admin/store/create.blade.php
+++ resources/views/admin/store/create.blade.php
@@ -24,8 +24,6 @@
     <div class="row">
         <div class="col-xl-12">
             <section class="hk-sec-wrapper">
-                {{-- <h5 class="hk-sec-title">Default Layout</h5> --}}
-                {{-- <p class="mb-25">More complex forms can be built using the grid classes. Use these for form layouts that require multiple columns, varied widths, and additional alignment options.</p> --}}
                 <div class="row">
                     <div class="col-sm">
                         @if (session('success'))
@@ -70,68 +68,6 @@
                     </div>
                 </div>
             </section>
-
-            {{-- <section class="hk-sec-wrapper">
-                <h5 class="hk-sec-title">Promoter Register</h5>
-                <p class="mb-25">Place an icon inside add-on on either side of an input. You may also place one on right side of an input.</p>
-                <div class="row">
-                    <div class="col-sm">
-                        <form>
-                            <div class="form-group">
-                                <label class="control-label mb-10" for="exampleInputuname_1">User Name</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="icon-user"></i></span>
-                                    </div>
-                                    <input type="text" class="form-control" id="exampleInputuname_1" placeholder="Username">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label mb-10" for="exampleInputEmail_1">Email address</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="icon-envelope-open"></i></span>
-                                    </div>
-                                    <input type="email" class="form-control" id="exampleInputEmail_1" placeholder="Enter email">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label mb-10" for="exampleInputEmail_1">Store Name</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="icon-envelope-open"></i></span>
-                                    </div>
-                                    <input type="email" class="form-control" id="exampleInputEmail_1" placeholder="Enter email">
-                                    <select class="form-control custom-select d-block w-100" id="country">
-                                        <option value="">Choose...</option>
-                                        <option>United States</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label mb-10" for="exampleInputpwd_1">Password</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="icon-lock"></i></span>
-                                    </div>
-                                    <input type="password" class="form-control" id="exampleInputpwd_1" placeholder="Enter email">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label class="control-label mb-10" for="exampleInputpwd_2">Confirm Password</label>
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="icon-lock"></i></span>
-                                    </div>
-                                    <input type="password" class="form-control" id="exampleInputpwd_2" placeholder="Enter email">
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-primary mr-10">Submit</button>
-                        </form>
-                    </div>
-                </div>
-            </section> --}}
-
         </div>
     </div>
     <!-- /Row -->
